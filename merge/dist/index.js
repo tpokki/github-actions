c2bd1fe9b101cdbf9b63c18a1e788a324e9beb79
Toni Pokki <tpokki@users.noreply.github.com>
2022-01-19 16:19:00 +0200

    merge action (#5)

---
+++ merge/dist/index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const { Octokit } = require("@octokit/action");
+
+const octokit = new Octokit();
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+const head = core.getInput('head');
+const base = core.getInput('base');
+
+core.info(`merging ${head} to ${base} ...`)
+
+octokit.request('POST /repos/{owner}/{repo}/merges', {
+    owner: owner,
+    repo: repo,
+    base: base,
+    head: head,
+}).catch(err => core.error(err));
