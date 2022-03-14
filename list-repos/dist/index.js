576c21e224c22560fdd3e99bcc4bf181dc8d4db6
Toni Pokki <tpokki@users.noreply.github.com>
2022-03-14 16:35:32 +0200

    replace gcp sa key with wip (#6)

--- list-repos/dist/index.js
+++ list-repos/dist/index.js
@@ -4,10 +4,17 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [owner, _repo] = process.env.GITHUB_REPOSITORY.split("/");
 const extractedField = core.getInput('extractedField');
+const topic = core.getInput('topic', { required: false });
 
-octokit.paginate("GET /orgs/{owner}/repos", {
-    owner,
+octokit.paginate(octokit.rest.repos.listForOrg, {
+    org: owner,
+    mediaType: {
+        previews: ["mercy-preview"],
+    }
 })
 .then(response =>
-    core.setOutput("repos", response.map(repo => repo[extractedField]).join()))
+    core.setOutput("repos", response
+        .filter(repo => !topic  || repo.topics.includes(topic))
+        .map(repo => repo[extractedField])
+        .join()))
 .catch(err => core.error(err));
